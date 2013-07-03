5670cd3eb0aa0845fd800e32ab55a49213a8cebb
lrntothink <lrntothink@gmail.com>
2013-07-03 09:30:24 +0800

    搜索文件改动，可以设置要搜索的内容，要搜索的文件类型，搜索的位置，搜索结果的存放位置，增加计时功能和文件数目统计功能。

--- SearchFile.java
+++ SearchFile.java
@@ -10,6 +10,9 @@ import java.io.UnsupportedEncodingException;
 
 public class SearchFile {
 	private String searchWord;
+	private String allowFileType;
+	private long totalFile = 0;
+	private long actualSearchFiles = 0;
 	PrintWriter   out   = null;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -18,8 +21,16 @@ public class SearchFile {
 		
 	}
 	private void doIt(){
+		//要搜索的内容
+		String searchword = "杨亮";
+		//要搜索的文件类型
+		allowFileType = " txt ini js jsp java xml htm html properties sql ";
+		//搜索的位置
+		String searchPath = "D:\\workspace\\lcpt1.2";
+		//搜索结果的存放位置
+		String searchResultPath = "d:/112.txt";
 		try {
-			out   =   new PrintWriter(new OutputStreamWriter(new FileOutputStream("d:/111.txt" ), "gbk"));
+			out   =   new PrintWriter(new OutputStreamWriter(new FileOutputStream(searchResultPath), "gbk"));
 		} catch (UnsupportedEncodingException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -27,21 +38,27 @@ public class SearchFile {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		String word = "double";
-		File file = new File("D:\\workspace\\lcpt");
-		setSearchWord(word);
-		searchFiles(file);
+		long beginTime = System.currentTimeMillis();
+		setSearchWord(searchword);
+		searchFiles(new File(searchPath));
+		long endTime = System.currentTimeMillis();
+		System.out.println(searchPath+"路径下，共有文件："+totalFile+"个");
+		System.out.println("实际搜索文件数："+actualSearchFiles+"个");
+		System.out.println("共花费时间："+(endTime - beginTime)+" 毫秒");
 		out.close();
 	}
-	public String getSearchWord() {
-		return searchWord;
-	}
-	public void setSearchWord(String searchWord) {
-		this.searchWord = searchWord;
-	}
+	
 	private void searchFiles(File filePath ){
 		if(filePath.isFile()){
-			searchSingleFile(filePath,this.getSearchWord());
+			totalFile++;
+			String filename = filePath.getName().toLowerCase();
+			if(filename.indexOf(".")!=-1){
+				String filetype = filename.substring(filename.lastIndexOf(".")+1).toLowerCase().trim();
+				if(allowFileType.contains(" "+filetype+" ")){
+					actualSearchFiles++;
+					searchSingleFile(filePath,this.getSearchWord());
+				}
+			}
 		}else if(filePath.isDirectory()){
 			File[] file = filePath.listFiles();
 			for(File f:file){
@@ -49,8 +66,12 @@ public class SearchFile {
 			}
 		}
 	}
+	/**
+	 * 在文件中搜索
+	 * @param f 搜索的文件名
+	 * @param searchWord 要搜索的内容
+	 */
 	private void searchSingleFile(File f,String searchWord) {
-		String fileContents = "";
 		try {
 			FileInputStream file = new FileInputStream(f);
 			BufferedReader in = new BufferedReader(new InputStreamReader(file,"gbk"));
@@ -65,11 +86,17 @@ public class SearchFile {
 				}
 			}
 			if(totalNums!=0){
-				out.println(f.getAbsolutePath()+"中总共"+lineNums+"行，共找到"+totalNums+"处\""+searchWord+"\"!");
+				out.println(f.getAbsolutePath()+"中总共 "+lineNums+" 行，共找到 "+totalNums+" 处\""+searchWord+"\"!");
 			}
 			in.close();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+	}
+	public String getSearchWord() {
+		return searchWord;
+	}
+	public void setSearchWord(String searchWord) {
+		this.searchWord = searchWord;
 	}
 }
